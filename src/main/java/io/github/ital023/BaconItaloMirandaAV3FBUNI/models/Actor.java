b31c66f7470be2067e91a21758b902bdd8a823a4
Ital023 <contadoitalo911@gmail.com>
2025-06-10 18:47:33 -0300

    feat: Actor model and Connection record to create a connection between an actor and movie

---
+++ src/main/java/io/github/ital023/BaconItaloMirandaAV3FBUNI/models/Actor.java
@@ -0,0 +1,27 @@
+package io.github.ital023.BaconItaloMirandaAV3FBUNI.models;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Actor {
+
+    private final String name;
+
+    private final Set<Connection> connections = new HashSet<>();
+
+    public Actor(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Set<Connection> getConnections() {
+        return connections;
+    }
+
+    public void addConnection(Actor actor, String movie) {
+        this.connections.add(new Connection(actor, movie));
+    }
+}
---
+++ src/main/java/io/github/ital023/BaconItaloMirandaAV3FBUNI/models/Connection.java
@@ -0,0 +1,6 @@
+package io.github.ital023.BaconItaloMirandaAV3FBUNI.models;
+
+public record Connection(Actor actor, String movie) {
+
+}
+
