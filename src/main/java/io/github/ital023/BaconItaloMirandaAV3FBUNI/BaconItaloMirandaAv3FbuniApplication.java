4c84687a8c817441599234ef18267f747d47e93b
Ital023 <contadoitalo911@gmail.com>
2025-06-10 18:43:36 -0300

    init: initial files

---
+++ src/main/java/io/github/ital023/BaconItaloMirandaAV3FBUNI/BaconItaloMirandaAv3FbuniApplication.java
@@ -0,0 +1,13 @@
+package io.github.ital023.BaconItaloMirandaAV3FBUNI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaconItaloMirandaAv3FbuniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaconItaloMirandaAv3FbuniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/ital023/BaconItaloMirandaAV3FBUNI/BaconItaloMirandaAv3FbuniApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ital023.BaconItaloMirandaAV3FBUNI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaconItaloMirandaAv3FbuniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
