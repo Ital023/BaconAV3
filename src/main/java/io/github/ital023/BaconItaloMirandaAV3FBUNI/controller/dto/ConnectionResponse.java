a342feb1bc97b8a930202eaf577d3f78feec0c12
Ital023 <contadoitalo911@gmail.com>
2025-06-10 20:05:15 -0300

    refact: turn into class to gain more acess and handle exceptions

--- src/main/java/io/github/ital023/BaconItaloMirandaAV3FBUNI/controller/dto/ConnectionResponse.java
+++ src/main/java/io/github/ital023/BaconItaloMirandaAV3FBUNI/controller/dto/ConnectionResponse.java
@@ -1,6 +1,33 @@
 package io.github.ital023.BaconItaloMirandaAV3FBUNI.controller.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.util.List;
 
-public record ConnectionResponse(String message, List<String> path) {
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ConnectionResponse {
+
+    private final String message;
+    private final List<String> path;
+
+    public ConnectionResponse(String message, List<String> path) {
+        this.message = message;
+        this.path = path;
+    }
+
+    public static ConnectionResponse found(List<String> path) {
+        return new ConnectionResponse("Conexão encontrada!", path);
+    }
+
+    public static ConnectionResponse notFound(String message) {
+        return new ConnectionResponse(message, null);
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public List<String> getPath() {
+        return path;
+    }
 }
